4977a183883d130dc60f01236c17f012abea2eb5
Harry <hjwp2@cantab.net>
2013-10-20 22:14:48 +0100

    Login view tdd'd in.

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('accounts.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
 )
