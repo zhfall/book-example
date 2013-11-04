0357fb80add3e2deaca7aec40301dd6fd346a6a3
Harry <hjwp2@cantab.net>
2013-11-04 23:04:56 +0000

    Screenshot-dumping on teardown.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,7 +1,11 @@
-from django.test import LiveServerTestCase
+from datetime import datetime
 from selenium import webdriver
 import sys
+
+from django.test import LiveServerTestCase
+
 from .server_tools import reset_database
+
 
 class FunctionalTest(LiveServerTestCase):
 
@@ -30,7 +34,32 @@ class FunctionalTest(LiveServerTestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        if not self._outcomeForDoCleanups.success:
+            self.take_screenshot()
+            self.dump_html()
+
         self.browser.quit()
+        super().tearDown()
+
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')
+        return '{}.{}-{}'.format(
+            self.__class__.__name__, self._testMethodName, timestamp
+        )
+
+
+    def take_screenshot(self):
+        filename = 'seleniumscreenshot-{}.png'.format(self._get_filename())
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+
+    def dump_html(self):
+        filename = 'seleniumhtml-{}.html'.format(self._get_filename())
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
 
 
     def get_item_input_box(self):
