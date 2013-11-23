1742767ed7c3c38ad001dab11b4a98a641c1aac6
Harry <hjwp2@cantab.net>
2013-11-23 15:26:59 +0000

    Tweaks to get tests passing, list sharing just about works now.

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
 from .base import FunctionalTest
 
+def get_browser_cleanup_function(browser):
+    def cleanup():
+        try: browser.quit()
+        except: pass
+    return cleanup
+
+
 class SharingTest(FunctionalTest):
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
         self.create_pre_authenticated_session('edith@email.com')
         edith_browser = self.browser
-        self.addCleanup(edith_browser.quit)
+        self.addCleanup(get_browser_cleanup_function(edith_browser))
 
         # Oniciferous is also hanging out on the lists site
         oni_browser = webdriver.Firefox()
-        self.addCleanup(oni_browser.quit)
+        self.addCleanup(get_browser_cleanup_function(oni_browser))
         self.browser = oni_browser
         self.create_pre_authenticated_session('oniciferous@email.com')
 
@@ -45,16 +52,16 @@ class SharingTest(FunctionalTest):
         # It says that it's edith's list
         self.wait_for(
             lambda: self.assertIn(
-                'edith@email.com',
-                self.browser.find_element_by_name('body').text
+                'List owner: edith@email.com',
+                self.browser.find_element_by_tag_name('body').text
             )
         )
 
         # He adds an item to the list
-        self.get_item_input_box().send_keys('Hi Edith!')
+        self.get_item_input_box().send_keys('Hi Edith!\n')
 
         # When edith refreshes the page, she sees Oniciferous's addition
         self.browser = edith_browser
         self.browser.refresh()
-        self.browser.find_elements_by_link_text('Hi Edith!')
+        self.check_for_row_in_list_table('2: Hi Edith!')
 
