ffea3a45ba1bf4fb33f31db5258a4754f4fc5bbc
Harry <hjwp2@cantab.net>
2013-11-25 19:26:58 +0000

    Patch screenshots onto ch. 7

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,7 +9,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        if not self._outcomeForDoCleanups.success:
+            self.take_screenshot()
+            self.dump_html()
+
         self.browser.quit()
+        super().tearDown()
+
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')
+        return '{}.{}-{}'.format(
+            self.__class__.__name__, self._testMethodName, timestamp
+        )
+
+
+    def take_screenshot(self):
+        filename = 'seleniumscreenshot-{}.png'.format(self._get_filename())
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+
+    def dump_html(self):
+        filename = 'seleniumhtml-{}.html'.format(self._get_filename())
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
 
 
     def check_for_row_in_list_table(self, row_text):
