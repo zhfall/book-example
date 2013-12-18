9252512114672f14ad6f113363961e72aaee2dec
Harry <hjwp2@cantab.net>
2013-12-18 13:05:41 +0000

    Page pattern, temporarily comment out stuff in my_lists.html

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,4 +1,5 @@
 from .base import FunctionalTest
+from .home_and_list_pages import HomePage
 
 class MyListsTest(FunctionalTest):
 
@@ -7,9 +8,8 @@ class MyListsTest(FunctionalTest):
         self.create_pre_authenticated_session(email='edith@email.com')
 
         # She goes to the home page and starts a list
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Reticulate splines\n')
-        self.get_item_input_box().send_keys('Immanentize eschaton\n')
+        list_page = HomePage(self).start_new_list('Reticulate splines')
+        list_page.add_item('Immanentize eschaton')
         first_list_url = self.browser.current_url
 
         # She notices a "My lists" link, for the first time.
@@ -18,26 +18,25 @@ class MyListsTest(FunctionalTest):
         # She sees that her list is in there, named according to its
         # first list item
         self.browser.find_element_by_link_text('Reticulate splines').click()
-        self.wait_for(
-            lambda: self.assertEqual(self.browser.current_url, first_list_url)
-        )
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.current_url, first_list_url
+        ))
 
         # She decides to start another list, just to see
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Click cows\n')
+        self.start_new_list('Click cows')
         second_list_url = self.browser.current_url
 
         # Under "my lists", her new list appears
         self.browser.find_element_by_link_text('My lists').click()
         self.browser.find_element_by_link_text('Click cows').click()
-        self.assertEqual(self.browser.current_url, second_list_url)
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.current_url, second_list_url
+        ))
 
         # She logs out.  The "My lists" option disappears
         self.browser.find_element_by_id('id_logout').click()
-        self.assertEqual(
+        self.wait_for(lambda: self.assertEqual(
             self.browser.find_elements_by_link_text('My lists'),
             []
-        )
-
-
+        ))
 
--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -23,43 +23,39 @@ class SharingTest(FunctionalTest):
 
         # Edith goes to the home page and starts a list
         self.browser = edith_browser
-        HomePage(self).start_new_list('Get help')
+        list_page = HomePage(self).start_new_list('Get help')
 
         # She notices a "Share this list" option
-        share_box = self.browser.find_element_by_css_selector('input[name=email]')
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your@friends-email.com'
         )
 
-        share_box.send_keys('oniciferous@email.com\n')
-
+        # She shares her list.
         # The page updates to say that it's shared with Oniciferous:
-        body_text = self.browser.find_element_by_tag_name('body').text
-        self.assertIn('Shared with', body_text)
-        self.assertIn('oniciferous@email.com', body_text)
+        list_page.share_list_with('oniciferous@email.com')
+
 
         # Oniciferous now goes to the 'My lists' page with his browser
         self.browser = oni_browser
-        self.browser.get(self.server_url)
-        self.browser.find_element_by_link_text('My lists').click()
+        home_page = HomePage(self).go_to_home_page()
+        home_page.go_to_my_lists_page()
 
-        # He sees edith's list in there!
+        # He sees Edith's list in there!
         self.browser.find_element_by_link_text('Get help').click()
 
-        # It says that it's edith's list
-        self.wait_for(
-            lambda: self.assertIn(
-                'List owner: edith@email.com',
-                self.browser.find_element_by_tag_name('body').text
-            )
-        )
+        # On the list page, Oniciferous can see says that it's Edith's list
+        self.wait_for(lambda: self.assertEqual(
+            list_page.get_list_owner(),
+            'edith@email.com'
+        ))
 
         # He adds an item to the list
-        self.get_item_input_box().send_keys('Hi Edith!\n')
+        list_page.add_new_item('Hi Edith!')
 
-        # When edith refreshes the page, she sees Oniciferous's addition
+        # When Edith refreshes the page, she sees Oniciferous's addition
         self.browser = edith_browser
         self.browser.refresh()
-        self.check_for_row_in_list_table('2: Hi Edith!')
+        list_page.wait_for_new_item_in_list('Hi Edith!', 2)
 
